591e3b5d93c28637471fd78b7735bd2990b333b0
Manav Sharma <manav157sh@gmail.com>
2025-09-04 16:29:27 +0530

    Added mark as Done and some updates sunctionalities learning Updates in Arrays

--- states-react2/src/ToDoList.jsx
+++ states-react2/src/ToDoList.jsx
@@ -1,23 +1,75 @@
 import React from "react";
 import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 export default function ToDoList() {
-  let [toDos, setTodos] = useState(["sample Task"]);
+  let [toDos, setTodos] = useState([
+    { task: "sample Task", id: uuidv4(), isDone: false },
+  ]);
   let [newTodo, setNewTodo] = useState("");
 
-// triggered when Add Task button is clicked
+  // triggered when Add Task button is clicked
   let addNewTask = () => {
     // console.log("added");
     setTodos((prev) => {
-      return [...prev, newTodo]; // create a new array with the previous tasks and the new task
+      //   if (newTodo.trim() === "") return; // prevent empty tasks
+      return [...prev, { task: newTodo, id: uuidv4() }]; // create a new array with the previous tasks and the new task
     });
     setNewTodo(""); // clear the input field after adding the task
   };
 
-// triggered when input field value changes
+  // triggered when input field value changes
   let updateTodoValue = (event) => {
     setNewTodo(event.target.value); // set the value of newTodo to the input field value
-    console.log({newTodo});
+    console.log({ newTodo });
+  };
+
+  let deleteTodo = (id) => {
+    // console.log({id});
+    // let copy = toDos.filter((todo) => todo.id != id); // create a new array without the task with the given id
+    // // console.log({copy});
+    // setTodos(copy); // update the state with the new array
+    setTodos((prev) => {
+      return prev.filter((todo) => todo.id !== id); // create a new array without the task with the given id
+    });
+  };
+
+  let upperCaseAll = () => {
+    let newArr = toDos.map((todo) => {
+      return {
+        ...todo,
+        task: todo.task.toUpperCase(),
+      };
+    });
+    setTodos(newArr); //pass the new array to setTodos
+  };
+
+  let upperCaseOne = (id) => {
+    let newArr = toDos.map((todo) => {
+      if (todo.id === id) {
+        return {
+          ...todo,
+          task: todo.task.toUpperCase(),
+        };
+      } else {
+        return todo;
+      }
+    });
+    setTodos(newArr);
+  };
+
+  let markAsDone = (id) => {
+    let newArr = toDos.map((todo) => {
+      if (todo.id === id) {
+        return {
+          ...todo,
+          isDone: true,
+        };
+      } else {
+        return todo;
+      }
+    });
+    setTodos(newArr);
   };
 
   return (
@@ -38,10 +90,22 @@ export default function ToDoList() {
 
       <ul>
         {/* // will loop through the toDos array and display each task as a list item */}
-        {toDos.map((todo, index) => (
-          <li key={index}>{todo}</li>
+        {/* react requires a unique key prop for each item in a list to help with efficient rendering */}
+        {toDos.map((todo) => 
+        (
+          <li key={todo.id}>
+            <span style={{ textDecoration: todo.isDone ? "line-through" : "none" }}>{todo.task}</span>
+            &nbsp; &nbsp;
+            <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+            &nbsp; &nbsp;
+            <button onClick={() => upperCaseOne(todo.id)}>UpperCase</button>
+            &nbsp; &nbsp;
+            <button onClick={() => markAsDone(todo.id)}>Mark as Done</button>
+          </li>
         ))}
       </ul>
+
+      <button onClick={upperCaseAll}>UpperCaseAll</button>
     </div>
   );
 }
